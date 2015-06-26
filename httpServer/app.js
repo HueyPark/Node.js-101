224875aa825f8c5d9c3496028fec685588391662
HueyPark <jaewan.huey.Park@gmail.com>
2015-06-26 23:30:07 +0900

    refactor: httpServer

--- httpServer/app.js
+++ httpServer/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
+function onRequest(req, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
     res.end('hello world\n');
-});
+}
 
-server.listen(9100);
+http.createServer(onRequest).listen(9100);
